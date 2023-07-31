a58a7899321fc10c0506615b5dae22153202467a
bradleymatjie <bradleymatjie.job@gmail.com>
2023-07-31 10:55:39 +0200

    categories

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -2,7 +2,6 @@ import './Hero.scss';
 import heroPlate from '../../assets/hero-plate.jpg';
 import Dessert from '../../assets/dessert.jpg';
 import Meaty from '../../assets/meaty.jpg';
-import Cocktails from '../../assets/cocktails.jpg';
 import Pastry from '../../assets/pastry.jpg';
 import Seafood from '../../assets/seafood.jpg';
 import Vegan from '../../assets/vegan.jpg';
@@ -24,82 +23,43 @@ export const Hero = () => {
             <img src={heroPlate} alt="Hero Plate" />
         </div>
     </section>
-    
-    <section className="categories-container">
-      <h2>Categories</h2>
-      <p>Take a glimpse at the variety of recipes we have for you</p>
-      <a href="#!">View All</a>
-      <div className="row">
 
-        <div className="food-item">
-        <img src={Meaty} alt="Meaty"></img>
-        <a href="#!">Meaty</a>
+    <section className='categories-container'>
+      <div className='top'>
+        <div>
+          <h2>Categories</h2>
+          <p>Take a glimpse at the variety of recipes we have for you</p>
         </div>
-
-        <div className="food-item">
-        <img src={Vegan} alt="Vegeterian/Vegan"></img>
-        <a href="#!">Vegeterian & Vegan</a>
-        </div>
-
-        <div className="food-item">
-        <img src={Pastry} alt="Pastry"></img>
-        <a href="#!">Pastry</a>
-        </div>
-    </div>
-
-      <div className="row">
-        <div className="food-item">
-        <img src={Seafood} alt="Seafood"></img>
-        <a href="#!">Seafood</a>
-        </div>
-
-        <div className="food-item">
-        <img src={Dessert} alt="Desserts"></img>
-        <a href="#!">Desserts</a>
-        </div>
-        
-        <div className="food-item">
-        <img src={Cocktails} alt="Cocktails"></img>
-        <a href="#!">Cocktails</a>
-        </div>
-
+        <button>View All</button>
       </div>
 
-    </section>
-      
-    <section>
-      <div className="section-image">
-        <img src={sectionImage} alt="meal"/>
+      <div className="cards">
+        <div className="card">
+          <img src={Meaty} alt="" />
+          <h3>Meaty</h3>
+        </div>
+
+        <div className="card">
+          <img src={Vegan} alt="" />
+          <h3>Vegan</h3>
+        </div>
+
+        <div className="card">
+          <img src={Seafood} alt="" />
+          <h3>SeaFood</h3>
+        </div>
+
+        <div className="card">
+          <img src={Pastry} alt="" />
+          <h3>Pastry</h3>
+        </div>
+
+        <div className="card">
+          <img src={Dessert} alt="" />
+          <h3>Dessert</h3>
+        </div>
       </div>
     </section>
-    <footer>
-      <div className="footer-container">
-        <div className="links">
-          <h3>Links</h3>
-        <ul>
-          <li>Home</li>
-          <li>Recipes</li>
-          <li>Contact Us</li>  
-        </ul>  
-        </div>
-        <div className="links">
-          <h3>Social</h3>
-        <ul>
-          <li>Instagram</li>
-          <li>Facebook</li>
-          <li>Pintrest</li>  
-        </ul>  
-        </div>
-        <div className="form">
-          <h3>Newsletter</h3>
-          <div className="input-box">
-          <input type="text" name="email" placeholder="Type in Email"></input>
-          <button type="submit">Subscribe</button>
-          </div>
-          </div>
-      </div>
-    </footer>
-
     </>
   )
 }
